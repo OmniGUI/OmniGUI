fcea16aa06f540dbdbb564ffa2ec87e3a80e405b
José Manuel Nieto Sánchez <superjmn@outlook.com>
2017-01-28 16:26:32 +0100

    Win2D

--- UwpApp/MainPage.xaml.cs
+++ UwpApp/MainPage.xaml.cs
@@ -1,22 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+﻿using Windows.UI.Xaml.Controls;
 
 namespace UwpApp
 {
+    using Microsoft.Graphics.Canvas.UI.Xaml;
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
@@ -25,6 +12,10 @@ namespace UwpApp
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void CanvasControl_OnDraw(CanvasControl sender, CanvasDrawEventArgs args)
+        {            
         }
     }
 }
