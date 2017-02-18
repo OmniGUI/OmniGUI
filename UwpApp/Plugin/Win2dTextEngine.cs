0bfe85694bd5c6d704f55f0f26c6142f0d3ff7b5
José Manuel Nieto Sánchez <superjmn@outlook.com>
2017-02-18 12:13:32 +0100

    Un poquito de tela para UWP

--- UwpApp/Plugin/Win2dTextEngine.cs
+++ UwpApp/Plugin/Win2dTextEngine.cs
@@ -5,34 +5,53 @@ using OmniGui;
 namespace UwpApp.Plugin
 {
     using System;
-    using Windows.UI.Xaml.Media;
+    using System.Collections.Generic;
     using OmniGui.Geometry;
 
     public class Win2DTextEngine : ITextEngine
     {
-        private CanvasDrawingSession session;
+        private const float NearInfinity = 10000;
+        private const string LineHeightProbe = "fg";
+        private CanvasDrawingSession drawingSession;
+        private readonly IDictionary<string, double> lineHeights = new Dictionary<string, double>();
 
         public void SetDrawingSession(CanvasDrawingSession session)
         {
-            this.session = session;
+            this.drawingSession = session;
         }
 
         public Size Measure(FormattedText formattedText)
         {
-            var constraintWidth = (float)(double.IsInfinity(formattedText.Constraint.Width) ? 1000 : formattedText.Constraint.Width);
-            var constraintHeight = (float)(double.IsInfinity(formattedText.Constraint.Height) ? 1000 : formattedText.Constraint.Height);
+            var constraintWidth = (float)(double.IsInfinity(formattedText.Constraint.Width) ? NearInfinity : formattedText.Constraint.Width);
+            var constraintHeight = (float)(double.IsInfinity(formattedText.Constraint.Height) ? NearInfinity : formattedText.Constraint.Height);
 
-            var t = new CanvasTextLayout(session, formattedText.Text, new CanvasTextFormat(), constraintWidth, constraintHeight);
+            var canvasTextFormat = new CanvasTextFormat
+            {
+                FontFamily = formattedText.FontFamily,
+                FontSize = formattedText.FontSize,
+                FontWeight = formattedText.FontWeight.ToWin2D(),
+            };
+
+            var t = new CanvasTextLayout(drawingSession, formattedText.Text, canvasTextFormat, constraintWidth, constraintHeight);
             return new Size(t.DrawBounds.Width, t.DrawBounds.Height);
         }
 
         public double GetHeight(string fontFamily)
         {
-            var canvasTextFormat = new CanvasTextLayout(session, "", new CanvasTextFormat(), 0, 0);
+            if (lineHeights.TryGetValue(fontFamily, out var height))
+            {
+                return height;
+            }
 
-            var fontDpiSize = 16;
-            var fontHeight = Math.Ceiling(fontDpiSize * 1.2);
-            return fontHeight;
+            var canvasTextFormat = new CanvasTextFormat
+            {
+                FontFamily = fontFamily,
+            };
+
+            var t = new CanvasTextLayout(drawingSession, LineHeightProbe, canvasTextFormat, NearInfinity, NearInfinity);
+            height = t.DrawBounds.Height;
+            lineHeights.Add(fontFamily, height);
+            return height;
 
         }
     }
