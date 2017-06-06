702ef3656044805d6373510f4288132b86c6bc97
José Manuel Nieto Sánchez <superjmn@outlook.com>
2017-06-06 09:42:11 +0200

    Avances iOS

---
+++ iOSApp/Omni/OmniGuiWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace iOSApp
+{
+    public class OmniGuiWindow : UIWindow
+    {
+        public OmniGuiWindow(CGRect mainScreenBounds)
+        {            
+            
+        }
+
+        public override void Draw(CGRect rect)
+        {
+            using (var context = UIGraphics.GetCurrentContext())
+            {
+                // Drawing code
+                CGRect rectangle = new CGRect(0, 100, 320, 100);
+                context.SetFillColor((nfloat) 1.0, (nfloat) 1.0, 0,  (nfloat) 0.0);
+                context.SetStrokeColor((nfloat) 0.0, (nfloat) 0.0, (nfloat) 0.0, (nfloat) 0.5);
+                context.FillRect(rectangle);
+                context.StrokeRect(rectangle);
+            }            
+        }
+    }
+}
